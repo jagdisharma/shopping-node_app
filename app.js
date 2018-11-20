364d3e948e3b9207975cd484e1d4bae56e34a14e
Jagdish Sharma <jagdishcsharma29@gmail.com>
2018-11-20 08:30:55 +0530

    first-commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const adminRoutes = require('./router/admin');
+const shopRoutes = require('./router/shop');
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(adminRoutes);
+app.use(shopRoutes);
+
+app.listen(3000);
